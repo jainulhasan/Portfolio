205a4ec0ee59ada2a687be97896158667436dbde
Jainul Hasan <jainulhasan11@gmail.com>
2022-06-16 19:11:37 +0530

    final commit

--- src/SocialFollow.jsx
+++ src/SocialFollow.jsx
@@ -10,9 +10,9 @@ export default function SocialFollow() {
   return (
     <div className="social-app">
       {/* <a href="#" ><i class="fab fa-twitter fa-2x"></i></a> */}
-      <a href="https://github.com/jainulhasan" target="__blank"><i class="fab fa-github fa-2x"></i></a>
-      <a href="https://www.linkedin.com/in/jainul-hasan " target="__blank"><i class="fab fa-linkedin fa-2x"></i></a>
-      <a href="https://www.instagram.com/jainul_hasan_" target="__blank"><i class="fab fa-instagram fa-2x"></i></a>
+      <a href="https://github.com/jainulhasan" target="_blank"><i class="fab fa-github fa-2x"></i></a>
+      <a href="https://www.linkedin.com/in/jainul-hasan " target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
+      <a href="https://www.instagram.com/jainul_hasan_" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
     </div>
   );
 }
